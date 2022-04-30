9e763f228fe54e61a04783dfde4b0db14ebbe96e
SonnyFixit <fikser@tlen.pl>
2022-04-30 23:46:09 +0200

    Models templates

---
+++ OrganizerApp/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace OrganizerApp.Models
+{
+    class Note
+    {
+        public int Id { get; set; }
+        public int NotebookID { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string FileLocation { get; set; }
+
+    }
+}
---
+++ OrganizerApp/Models/Notebook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrganizerApp.Models
+{
+    class Notebook
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
